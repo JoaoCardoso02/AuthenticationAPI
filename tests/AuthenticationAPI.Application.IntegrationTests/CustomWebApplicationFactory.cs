7cfe0b3a67e69cf2b83f60c989313027fb8b87ca
joao <joaolucas.cardoso02@gmail.com>
2023-07-11 07:23:56 -0300

    feat: add account unit test

---
+++ tests/AuthenticationAPI.Application.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using AuthenticationAPI.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthenticationAPI.Application.IntegrationTests;
+
+public class FakeApplicationDbContext : ApplicationDbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseInMemoryDatabase("in-memory");
+    }
+
+}
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+
+        base.ConfigureWebHost(builder);
+
+        builder.ConfigureServices((builder, services) =>
+        {
+            // 	[195]	{AuthenticationAPI.Infrastructure.Common.Interfaces.IApplicationDbContext}	System.RuntimeType
+            //var a = services.Select((d) => d.ServiceType);
+            //var b = typeof(DbContextOptions<ApplicationDbContext>);
+            //var serviceDescriptor = services.SingleOrDefault(d => {
+            //    Console.WriteLine(d);
+            //    return d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>);
+            //});
+
+            //if (serviceDescriptor != null)
+            //    services.Remove(serviceDescriptor);
+            //ApplicationDbContext fakeApplicationDbContext = new FakeApplicationDbContext();
+            //fakeApplicationDbContext.Database.EnsureCreated();
+            //services.AddSingleton<ApplicationDbContext>(fakeApplicationDbContext);
+        });
+    }
+}
+
---
+++ tests/AuthenticationAPI.Domain.UnitTests/Entities/Account.Unit.cs
@@ -0,0 +1,74 @@
+﻿using AuthenticationAPI.Domain.Entities;
+using AuthenticationAPI.Domain.ValueObjects;
+
+namespace AuthenticationAPI.Domain.UnitTests.Entities;
+
+public class AccountUnit
+{
+    [Test]
+    public void ShouldInstanceAccountSuccessfully() {
+        Email email = Email.Create("valid@email.com");
+        Account account = new Account(email, "strong-password");
+
+        Assert.IsInstanceOf<Account>(account);
+    }
+
+    [Test]
+    public void ShouldReturnEmailSuccessfully() {
+        Email email = Email.Create("valid@email.com");
+        Account account = new Account(email, "strong-password");
+
+        Assert.Multiple(() => {
+            Assert.IsNotEmpty(account.Email);
+            Assert.That(email, Is.EqualTo(account.Email));
+        });
+    }
+
+    [Test]
+    public void ShouldReturnPasswordSuccessfully() {
+        Email email = Email.Create("valid@email.com");
+        string password = "strong-password";
+        Account account = new Account(email, password);
+
+        Assert.Multiple(() => {
+            Assert.IsNotEmpty(account.Password);
+            Assert.That(password, Is.EqualTo(account.Password));
+        });
+    }
+
+    [Test]
+    public void ShouldInitCreatedAtFieldAsCurrentDateTime() {
+        Email email = Email.Create("valid@email.com");
+        Account account = new Account(email, "strong-password");
+
+        Assert.Multiple(() => {
+            Assert.NotNull(account.CreatedAt);
+            Assert.IsInstanceOf<DateTime>(account.CreatedAt);
+        });
+    }
+
+    [Test]
+    public void ShouldChangeOnlyWritableFieldsSucessfully() {
+        Email email = Email.Create("valid@email.com");
+        Account account = new Account(email, "strong-password");
+
+        int? OldId = account.Id;
+        DateTime OldCreatedAt = account.CreatedAt;
+        DateTime? OldDeletedAt = account.DeletedAt;
+
+        account.Id = 2;
+        account.CreatedAt = DateTime.Now;
+        account.DeletedAt = DateTime.Now;
+
+        Assert.Multiple(() => {
+            Assert.That(OldId, Is.Not.EqualTo(account.Id));
+            Assert.That(OldCreatedAt, Is.Not.EqualTo(account.CreatedAt));
+            Assert.That(OldDeletedAt, Is.Not.EqualTo(account.DeletedAt));
+
+            Assert.That(account.Id, Is.EqualTo(2));
+            Assert.NotNull(account.CreatedAt);
+            Assert.NotNull(account.DeletedAt);
+        });
+    }
+}
+
